375edac41343b366ccf59a5baab23c110f43dc11
Wojciech Szoda <wojciechszoda@icloud.com>
2021-12-10 10:04:52 +0100

    d

--- src/components/ToDo/ToDoList.js
+++ src/components/ToDo/ToDoList.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useState } from "react";
 
 import ToDoControls from "./ToDoControls";
+import Button from "../Buttons/Button";
 
 const ToDoList = () => {
     const [toDos, setToDos] = useState([]);
     const [key, SetKey] = useState(0);
 
-    function AddToDo(ToDo) {
-        setToDos(toDos.concat([ToDo]));
+    function AddToDo(toDo) {
+        setToDos(toDos.concat([{key:GetKey(), task:toDo}]));
     }
 
     function GetKey(){
@@ -16,12 +17,21 @@ const ToDoList = () => {
         return key;
     }
 
+    const deleteTask = (taskKey) =>{
+        setToDos(toDos.filter(toDo => toDo.key !== taskKey));
+    }
+
     return(
         <div>
             <ul>
                 {
                     toDos.map((Task) => {
-                        return <li key={GetKey}>{Task}</li>
+                        return <li key={Task.key}>
+                            <div>
+                                <p>{Task.task}</p>
+                                <Button function={deleteTask(Task.key)} text="Remove Task"/>
+                            </div>
+                        </li>
                     })
                 }
             </ul>
